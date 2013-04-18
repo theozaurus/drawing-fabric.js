8c39d99c383385488b6f7c0c7a8b8a80a76991f6
Theo Cushion <theo@jivatechnology.com>
2013-04-18 17:05:49 +0100

    Feature: Added support for selecting blocks of text

--- src/drawing_fabric.js
+++ src/drawing_fabric.js
@@ -542,6 +542,26 @@ DrawingFabric.Functionality.addText = (function(){
 
   var utils = DrawingFabric.utils;
 
+  var fixTabKey = function(e){
+    if(e.keyCode == 9){
+      // get caret position/selection
+      var start = this.selectionStart;
+      var end   = this.selectionEnd;
+
+      var $this = $(this);
+      var value = $this.val();
+
+      // set textarea value to: text before caret + tab + text after caret
+      $this.val(value.substring(0, start) + "\t" + value.substring(end));
+
+      // put caret at right position again (add one for the tab)
+      this.selectionStart = this.selectionEnd = start + 1;
+
+      // prevent the focus lose
+      e.preventDefault();
+    }
+  };
+
   return function(){
 
     this.initialize = function(){
@@ -554,8 +574,9 @@ DrawingFabric.Functionality.addText = (function(){
       var createHiddenInput = function(){
         if($hiddenInput){return;}
         $hiddenInput = $('<textarea></textarea>');
-        // $hiddenInput.css('position','absolute');
-        // $hiddenInput.css('left','-900000px');
+        $hiddenInput.css('position','absolute');
+        $hiddenInput.css('left','-900000px');
+        $hiddenInput.keydown(fixTabKey);
         $(that.fabricCanvas.wrapperEl).append($hiddenInput);
       };
       createHiddenInput();
@@ -585,63 +606,125 @@ DrawingFabric.Functionality.addText = (function(){
         ctx.restore();
       };
 
-      var canvasPositionToCursorIndex = function(x,y){
+      var selectionToSelectionByLine = function(indexes){
+        var text = selectedObj.text;
 
+        var lines = [];
+        // Iterate over the characters to create an array of start and end
+        // points for each line (we need a selection box for each line)
+        for(var i=indexes[0]; i <= indexes[1]; i++){
+          if(i == indexes[0]){
+            lines.unshift({start: i});
+          }
+          if(i == indexes[1]){
+            lines[0].end = i;
+            break;
+          }
+          if(text[i] == '\n'){
+            lines[0].end = i;
+            lines.unshift({start: i+1});
+          }
+        }
+
+        return lines;
       };
 
       var cursor;
       var cursorBlinker;
-      var lastCoords;
-      var renderCursor = function(coords){
-        if(coords != lastCoords){
-          removeCursor();
-          cursor = new fabric.Rect({
-            top:     coords.y,
-            left:    coords.x,
-            originY: 'top',
-            originX: 'left',
-            height:  coords.dy,
-            width:   1,
-            angle:   coords.angle
+      var renderCaratCursor = function(indexes){
+        var coords = cursorIndexToCanvasPosition(indexes[0]);
+
+        removeCarats();
+
+        cursor = new fabric.Rect({
+          top:        coords.y,
+          left:       coords.x,
+          originY:    'top',
+          originX:    'left',
+          height:     coords.dy,
+          width:      1,
+          angle:      coords.angle,
+          selectable: false
+        });
+        cursorBlinker = setInterval(function(){
+          cursor.set('opacity', !cursor.opacity ? 1 : 0);
+          that.fabricCanvas.renderAll();
+        }, 500);
+        that.fabricCanvas.add(cursor);
+        lastCoords = coords;
+      };
+
+      var selections = [];
+      var renderCaratSelection = function(indexes){
+        var lines = selectionToSelectionByLine(indexes);
+
+        removeCarats();
+
+        // For each line build a selection box
+        for(var j=0; j < lines.length; j++){
+          var line        = lines[j];
+          var coordsStart = cursorIndexToCanvasPosition(line.start);
+          var coordsEnd   = cursorIndexToCanvasPosition(line.end);
+
+          var width = utils.magnitudeBetweenPoints(
+            coordsStart.x,
+            coordsStart.y,
+            coordsEnd.x,
+            coordsEnd.y
+          );
+
+          // Get start and end of selection box
+          var selection = new fabric.Rect({
+            top:        coordsStart.y,
+            left:       coordsStart.x,
+            originY:    'top',
+            originX:    'left',
+            opacity:    0.5,
+            height:     coordsStart.dy,
+            width:      width,
+            angle:      coordsStart.angle,
+            selectable: false
           });
-          cursorBlinker = setInterval(function(){
-            cursor.set('opacity', !cursor.opacity ? 1 : 0);
-            that.fabricCanvas.renderAll();
-          }, 500);
-          that.fabricCanvas.add(cursor);
-          lastCoords = coords;
+
+          that.fabricCanvas.add(selection);
+          selections.push(selection);
         }
       };
 
-      var removeCursor = function(){
+      var removeCarats = function(){
         if(cursor){
           that.fabricCanvas.remove(cursor);
           clearInterval(cursorBlinker);
+          cursor = null;
         }
+        for(var i = 0; i < selections.length; i++){
+          that.fabricCanvas.remove(selections[i]);
+        }
+        selections = [];
       };
 
       var cursorIndexToCanvasPosition = function(cursorIndex){
         var dx,dy,x,y;
 
-        withContext(function(ctx){
-          var text = selectedObj.text;
+        var text = selectedObj.text;
 
-          // Iterate over the character to find which line the index is on
-          // and how many characters in it is
-          var newLineCount      = 0;
-          var charsSinceNewLine = 0;
+        // Iterate over the characters to find which line the index is on
+        // and how many characters in it is
+        var newLineCount      = 0;
+        var charsSinceNewLine = 0;
 
-          for(var i=0; i < cursorIndex; i++){
-            if(text[i] == '\n'){
-              newLineCount += 1;
-              charsSinceNewLine = 0;
-            } else {
-              charsSinceNewLine += 1;
-            }
+        for(var i=0; i < cursorIndex; i++){
+          if(text[i] == '\n'){
+            newLineCount += 1;
+            charsSinceNewLine = 0;
+          } else {
+            charsSinceNewLine += 1;
           }
+        }
 
-          var snippet = text.slice(cursorIndex - charsSinceNewLine,cursorIndex);
+        var snippet = text.slice(cursorIndex - charsSinceNewLine,cursorIndex);
 
+        withContext(function(ctx){
           dx = ctx.measureText(snippet).width;
           dy = selectedObj.fontSize * selectedObj.lineHeight;
 
@@ -667,17 +750,26 @@ DrawingFabric.Functionality.addText = (function(){
       };
 
       var cursorIndex = function(){
-        return $hiddenInput[0].selectionStart;
+        return [$hiddenInput[0].selectionStart,$hiddenInput[0].selectionEnd];
       };
 
-      var handleCursor = function(){
-        renderCursor(cursorIndexToCanvasPosition(cursorIndex()));
+      var lastIndexes;
+      var handleCarat = function(){
+        var indexes = cursorIndex();
+        if(indexes != lastIndexes){
+          if(indexes[0] == indexes[1]){
+            renderCaratCursor(indexes);
+          } else {
+            renderCaratSelection(indexes);
+          }
+          lastIndexes = indexes;
+        }
       };
 
       $hiddenInput.keyup(function(e){
         if(selectedObj){
           selectedObj.set('text',$hiddenInput.val());
-          handleCursor();
+          handleCarat();
           that.fabricCanvas.renderAll();
         }
       });
@@ -685,18 +777,18 @@ DrawingFabric.Functionality.addText = (function(){
       this.fabricCanvas.on('object:dblselected', function(event){
         selectedObj = activeTextObject(event.target);
         if(selectedObj){
-          handleCursor();
+          handleCarat();
           setupHiddenInput(selectedObj);
         }
       });
 
       this.fabricCanvas.on('selection:cleared', function(event){
         selectedObj = null;
-        removeCursor();
+        removeCarats();
       });
 
       var update = function(){
-        if(selectedObj){ setupHiddenInput(selectedObj); handleCursor(); }
+        if(selectedObj){ setupHiddenInput(selectedObj); handleCarat(); }
       };
 
       this.fabricCanvas.on('object:scaling',  update);
