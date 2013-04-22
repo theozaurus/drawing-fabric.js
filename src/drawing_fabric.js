5161e5c1de69dbd80ba06111af26f169e27127e5
Theo Cushion <theo@jivatechnology.com>
2013-04-22 11:42:49 +0100

    Feature: Extended tool functionality to allow button selection styling to work

--- src/drawing_fabric.js
+++ src/drawing_fabric.js
@@ -162,30 +162,44 @@ DrawingFabric.Functionality.tools = (function(){
 
   return function(config){
 
-    var tool = 'cursor';
+    var tool;
 
     this.initialize = function(){
 
       var that = this;
 
-      config.cursor.click(    function(){ that.tool('cursor');    });
-      config.ellipse.click(   function(){ that.tool('ellipse');   });
-      config.rectangle.click( function(){ that.tool('rectangle'); });
-      config.triangle.click(  function(){ that.tool('triangle');  });
-      config.line.click(      function(){ that.tool('line');      });
-      config.draw.click(      function(){ that.tool('draw');      });
-      config.arc.click(       function(){ that.tool('arc');       });
-      config.text.click(      function(){ that.tool('text');      });
+      var select = function(tool){
+        $tool_elements.removeClass('active');
+        config[tool].addClass('active');
+      };
+
+      var $tool_elements = $();
+      build_tool = function(tool,elements){
+        $tool_elements = $tool_elements.add(elements);
+        elements.click( function(){ that.tool(tool); } );
+      };
+
+      build_tool('cursor',    config.cursor);
+      build_tool('ellipse',   config.ellipse);
+      build_tool('rectangle', config.rectangle);
+      build_tool('triangle',  config.triangle);
+      build_tool('line',      config.line);
+      build_tool('draw',      config.draw);
+      build_tool('arc',       config.arc);
+      build_tool('text',      config.text);
 
       this.tool = function(t){
         if(t && t != tool){
           tool = t;
+          select(tool);
           that.fabricCanvas.fire('tool:change',tool);
           return tool;
         } else {
           return tool;
         }
       };
+
+      this.tool('cursor');
     };
 
   };
